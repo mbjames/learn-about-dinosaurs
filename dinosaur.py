e4c6257eda9eabeb275d91670bd10af7e7e43f3e
Michael J Bailey <mbjames203@gmail.com>
2021-10-12 18:42:58 -0700

    Dinosaurs added

--- dinosaur.py
+++ dinosaur.py
@@ -1,10 +1,7 @@
-import banner
 import menu
 
 
 def start():
-    banner.print_banner()
-
     menu.main_menu()
 
     answer = input('\n > ').lower()
@@ -40,6 +37,70 @@ def start():
     elif answer == '5':
         print('\n This small meat-eater was one of the earliest dinosaurs.')
         print(' It was fast and agile and would have fed on animals like small reptiles and insects.')
+
+        start()
+
+    elif answer == '6':
+        print('\n As portrayed in the Jurassic Park movies,Velociraptor was recreated at twice its actual size and '
+              ' closely modelled on Deinonychus.')
+        print(' Though this was seen as unscientific at the time, soon after the first film was released, a dinosaur '
+              ' of the same type,')
+        print(' Utahraptor, was discovered, even larger than the virtual Velociraptors.')
+
+        start()
+
+    elif answer == '7':
+        print('\n Ceratosaurus means horned lizard.')
+        print(' The dinosaur was given this name because it had a row of sharp horns on its head and a row of small, '
+              ' bony pieces of armour running along its back.')
+        print(' It is not known what this body armour was for, but it could have been for protection from attack by '
+              ' other Ceratosaurus or larger theropods such as Allosaurus and Torvosaurus.')
+
+        start()
+
+    elif answer == '8':
+        print('\n Brachiosaurus held its head very high.')
+        print(' It is likely to have eaten the leaves on tall tree-like plants.')
+
+        start()
+
+    elif answer == '9':
+        print('\n A feathered dinosaur. One species, Microraptor gui had long flight feathers on all 4 limbs.')
+        print(' It may have been capable of guided flight.')
+
+        start()
+
+    elif answer == '10':
+        print('\n Albertosaurus was a close relative of Tyrannosaurus,')
+        print(' but smaller and not as heavily built.')
+
+        start()
+
+    elif answer == '11':
+        print('\n Utahraptor is similar to Velociraptor, but much larger.')
+
+        start()
+
+    elif answer == '12':
+        print('\n The teeth of Allosaurus were 5-10cm long and curved backwards to prevent prey from escaping.')
+
+        start()
+
+    elif answer == '13':
+        print('\n One of the largest armoured dinosaurus, Ankylosaurus had a wide,')
+        print(' heavily armoured skull and a large tail club.')
+        print(' It had a large gut space for digesting plant material.')
+
+        start()
+
+    elif answer == '14':
+        print('\n Considered to be distinct from Brachiosaurus by some scientists,')
+        print(' Giraffatitan is known from the fossils of several individuals.')
+
+        start()
+
+    elif answer == '15':
+        print('\n Megalosaurus was one of the first dinosaurs discovered.')
 
         start()
 
